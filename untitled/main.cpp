b2a5d7995ad06123476eb2e3dd03ba5e120aaa1e
Ali229 <alinaeem229@hotmail.com>
2017-04-14 13:02:33 -0400

    Change

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -4,16 +4,13 @@
 using namespace std;
 int main ()
 {
-    int myints[]= {17,89,7,14};
-    std::list<int> mylist (myints,myints+4);
-    mylist.clear();
-    //mylist.remove(89);
-
-    std::cout << "mylist contains:";
-    for (std::list<int>::iterator it=mylist.begin(); it!=mylist.end(); ++it)
-        std::cout << ' ' << *it;
-    std::cout << '\n';
-
+    int x = 0;
+    std::list<int> mylist;
+    for (int i = 0; i < 100000000 ; ++i) {
+        mylist.push_back(100);
+    }
+    cin>>x;
+    cout<< "hi"<<endl;
     return 0;
 }
 
