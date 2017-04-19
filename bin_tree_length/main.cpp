74995823f08f02577fa9ea482c4dd6f1ca484186
Ali229 <alinaeem229@hotmail.com>
2017-04-19 00:30:20 -0400

    Added common questions

--- bin_tree_length/main.cpp
+++ bin_tree_length/main.cpp
@@ -2,12 +2,20 @@
 #include "bintree.h"
 using namespace std;
 using namespace main_savitch_10;
-void createTree() {
 
-
+int num_even(const binary_tree_node<int>* node) {
+    if (node == NULL) {
+        return 0;
+    }
+    int ans = 0;
+    if (node->data()%2 == 0) {
+        ans = 1;
+    }
+    return ans+num_even(node->left()) + num_even(node->right());
 }
 template <class Item>
 unsigned int path_length(binary_tree_node<Item>* root, const Item& target) {
+    assert(root != NULL);
     unsigned int path = 0;
     binary_tree_node<Item> *cursor = root;
     if (cursor == NULL) {
@@ -35,15 +43,44 @@ unsigned int path_length(binary_tree_node<Item>* root, const Item& target) {
 
 
 }
+bool similar(binary_tree_node<int>* root1, binary_tree_node<int>* root2) {
+    /*1. both empty */
+    if (root1==NULL && root2==NULL)
+        return 1;
+
+    /* 2. both non-empty -> compare them */
+    if (root1!=NULL && root2!=NULL)
+    {
+        return
+                (
+                        root1->data() == root2->data() &&
+                        similar(root1->left(), root2->left()) &&
+                        similar(root1->right(), root2->right())
+                );
+    }
+
+    /* 3. one empty, one not -> false */
+    return 0;
+}
 int main() {
     binary_tree_node<int> *b0 = new binary_tree_node<int>(0);
     binary_tree_node<int> *bb = new binary_tree_node<int>(1, b0, NULL);
     binary_tree_node<int> *bc = new binary_tree_node<int>(3);
     binary_tree_node<int> *b = new binary_tree_node<int>(2, bb, bc);
-    binary_tree_node<int> *b1 = new binary_tree_node<int>(5);
-    binary_tree_node<int> *b2 = new binary_tree_node<int>(4,b,b1);
+    binary_tree_node<int> *bd = new binary_tree_node<int>(5);
+    binary_tree_node<int> *b2 = new binary_tree_node<int>(4,b,bd);
     binary_tree_node<int> *b3 = new binary_tree_node<int>(10,b2,NULL);
+
+    binary_tree_node<int> *b01 = new binary_tree_node<int>(0);
+    binary_tree_node<int> *bb1 = new binary_tree_node<int>(1, b01, NULL);
+    binary_tree_node<int> *bc1 = new binary_tree_node<int>(3);
+    binary_tree_node<int> *b1 = new binary_tree_node<int>(2, bb1, bc1);
+    binary_tree_node<int> *bd1 = new binary_tree_node<int>(5);
+    binary_tree_node<int> *b21 = new binary_tree_node<int>(4,b1,bd1);
+    binary_tree_node<int> *b31 = new binary_tree_node<int>(10,b21,NULL);
     print(b3, 0);
-    cout<< path_length(b3, 4)<<endl;
+    //cout<< path_length(b3, 0)<<endl;
+    //cout<< num_even(b3) <<endl;
+    cout<<similar(b3, b31) <<endl;
     return 0;
 }
