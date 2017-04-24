367183e5924bd609655f14bed59165f881718203
Ali229 <alinaeem229@hotmail.com>
2017-04-24 12:05:29 -0400

    Assignment# 11 Added

---
+++ Assignment_11/cmake-build-debug/CMakeFiles/Assignment_11.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment_11.dir/main.cpp.o"
+  "Assignment_11.pdb"
+  "Assignment_11.exe"
+  "libAssignment_11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Assignment_11/main.cpp
+++ Assignment_11/main.cpp
@@ -1,19 +1,23 @@
+//Author: Muhammad Ali
+//Assignment# 11
+//Shell Sort
+#include <cstdlib>
 #include <iostream>
 
 using namespace std;
 void shellSort(int array[], int size) {
     int j, temp, gap = size/2;
     while (gap > 0) {
-    for (int i = 0; i+gap <= size; i++){
-        j = i;
-        while (j-gap>= 0 && array[j] < array[j-gap]){
-            temp = array[j];
-            array[j] = array[j-gap];
-            array[j-gap] = temp;
-            j = j-gap;
+        for (int i = 0; i+gap <= size; i++){
+            j = i;
+            while (j-gap>= 0 && array[j] < array[j-gap]){
+                temp = array[j];
+                array[j] = array[j-gap];
+                array[j-gap] = temp;
+                j = j-gap;
+            }
         }
-    }
-    gap = gap/2;
+        gap = gap/2;
     }
 }
 
@@ -22,7 +26,9 @@ int main() {
     int array[SIZE];
     //Filling array and printing unsorted
     for (int j = 0; j < SIZE; ++j) {
+        //srand (time(NULL));
         array[j] = rand() % 101 + 0;
+
         cout << array[j] << " ";
     }
     cout<< endl;
